797eb9a797a2081fcafbf2783fb2fc6e852f71c7
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-04-03 14:19:12 +0300

    add welcome module scr/csi.js and change brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 
+import welcomeUser from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+welcomeUser();
 
---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const welcomeUser = () => {
+const name = readlineSync.question('May I have your name? ');
+
+return console.log('Hello, ' + name + '!');
+	
+};
+
+export default welcomeUser;
+
