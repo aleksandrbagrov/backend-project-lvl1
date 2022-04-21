e5527ac4b0a41e6c5ef8e5ce4fd2d38e04b82f80
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-04-21 12:13:44 +0300

    fix bug in gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -1,5 +1,8 @@
 // gcd - (greatest common divisor) - determines the GCD of two integers
 const gcd = (num1, num2) => {
+  if (num1 === 0) {
+    return num2;
+  }
   const residue = num1 % num2;
   if (residue === 0) {
     return num2;
