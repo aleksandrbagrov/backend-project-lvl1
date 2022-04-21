dc6434882096a0d4026f6f871d0e1533f6870a49
Aleksandr Bagrov <bay@balticum-tv.lt>
2022-04-21 13:15:30 +0300

    modify index.js to resolve eslint problems

--- src/index.js
+++ src/index.js
@@ -5,9 +5,8 @@ const index = (questionOfGame, vinAnswer, name, j, numberOfAttempts) => {
   console.log(questionOfGame);
   const playerAnswer = answer('Your answer:');
   if (vinAnswer !== playerAnswer) {
-    console.log(
-      `\x1b[0;31m'${playerAnswer}' \x1b[0mis wrong answer ;(\x1b[0;32m. \x1b[0mCorrect answer was \x1b[0;31m'${vinAnswer}'\x1b[0;32m.\x1b[0m`
-    );
+    console.log(`\x1b[0;31m'${playerAnswer}' \x1b[0mis wrong answer ;(\x1b[0;32m. \x1b[0m\
+Correct answer was \x1b[0;31m'${vinAnswer}'\x1b[0;32m.\x1b[0m`);
     console.log(`Let\x1b[0;31m's try again, ${name}!\x1b[0m`);
     return false;
   }
